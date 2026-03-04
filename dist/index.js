8bec42bca27bb61891976d3116e8d686060db5ad
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-03-04 22:48:47 +0530

    rebuild main entry point dist output

--- dist/index.js
+++ dist/index.js
@@ -1,65 +1,10 @@
 #!/usr/bin/env node
-const { Command } = require('commander');
-const program = new Command();
-program
-    .command('greet veer')
-    .action((name) => {
-    console.log(`Hello ${name}!`);
-});
-program
-    .command("add <n1> <n2>")
-    .action((n1, n2) => {
-    const sum = Number(n1) + Number(n2);
-    console.log(sum);
-});
-program
-    .command("multiply <n1> <n2>")
-    .action((n1, n2) => {
-    const multiply = Number(n1) * Number(n2);
-    console.log(multiply);
-});
-program
-    .command("divide <n1> <n2>")
-    .action((n1, n2) => {
-    if (n2 === 0) {
-        console.log("❌ Cannot divide by zero");
-        return;
-    }
-    const divide = Number(n1) / Number(n2);
-    console.log(divide);
-});
-program
-    .command("floor <n1> <n2>")
-    .action((n1, n2) => {
-    const floor = Math.floor(Number(n1) / Number(n2));
-    console.log(floor);
-});
-program
-    .command("ceil <n1> <n2>")
-    .action((n1, n2) => {
-    const ceil = Math.ceil(Number(n1) / Number(n2));
-    console.log(ceil);
-});
-program
-    .command("mod <n1> <n2>")
-    .action((n1, n2) => {
-    console.log(Number(n1) % Number(n2));
-});
-program
-    .command("pow <base> <exp>")
-    .action((base, exp) => {
-    console.log(Math.pow(Number(base), Number(exp)));
-});
-program
-    .command("sqrt <n>")
-    .action((n) => {
-    console.log(Math.sqrt(Number(n)));
-});
-program
-    .command("percent <value> <total>")
-    .action((value, total) => {
-    value = Number(value);
-    total = Number(total);
-    console.log((value / total) * 100 + "%");
-});
-program.parse();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CLI_Engine = require("./cliEngine/cliEngine");
+const Greet_Command = require("./commands/greet");
+const Joke = require("./commands/joke");
+const Math_Commands = require("./commands/math");
+const engine = new CLI_Engine();
+engine.registerCommands([Greet_Command, Joke, Math_Commands]);
+engine.run();
