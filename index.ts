609166240a0bb83874a8cdb19c07303b072e2851
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-03-04 22:52:26 +0530

    register github and quote commands in entry point

--- index.ts
+++ index.ts
@@ -4,8 +4,10 @@ const CLI_Engine = require("./cliEngine/cliEngine");
 const Greet_Command = require("./commands/greet");
 const Joke = require("./commands/joke");
 const Math_Commands = require("./commands/math");
+const Github_Command = require("./commands/github");
+const Quote_Command = require("./commands/quote");
 
 const engine = new CLI_Engine();
 
-engine.registerCommands([Greet_Command, Joke, Math_Commands]);
+engine.registerCommands([Greet_Command, Joke, Math_Commands, Github_Command, Quote_Command]);
 engine.run();
